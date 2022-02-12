9aad402dc82d8c1b7e1573bfa00237654c914cab
chul0721 <chul070721@naver.com>
2022-02-12 22:00:57 +0900

    feat: improved syntax

--- src/index.ts
+++ src/index.ts
@@ -4,70 +4,70 @@ import path from "path"
 import fs from "fs"
 
 const variables: { [key: string]: any } = {}
-const statements = ["ㅇㅉ", "ㅈㅉ", "어쩔", "티비"]
+const statements = ["ㅇㅉ", "ㅌㅂ", "저쩔", "어쩔"]
 
 const execute = async (code: string) => {
-  const lines: string[] = code.split("\n") // ㅇㅋ 패치 함
-  if (lines[0] !== "어쩔티비") throw new Error("아무것도 모르죠?")
-  if (lines[lines.length - 1] !== "저쩔티비") throw new Error("아무것도 모르죠?")
-  lines.shift()
-  lines.pop()
+  const lines: string[] = code.split("\n")
+  if (lines.shift() !== "쿠쿠루삥뽕") throw new Error("아무것도 모르죠?")
+  if (lines.pop() !== "슉슈슉슉") throw new Error("아무것도 모르죠?")
   for (const line of lines) {
     const [statement] = line.split(" ")
-    if (statement === "ㅇㅉ") {
-      allocateVariable(line)
-    } else if (statement === "어쩔") {
-      print(line)
-    } else if (statement === "티비") {
-      input(line)
-    } else if (statement === "ㅇㅇ") {
+    if (statement === "어쩔") {
+      declareVariable(line)
+    } else if (statement === "저쩔") {
       assignVariable(line)
+    } else if (statement === "ㅇㅉ") {
+      print(line)
+    } else if (statement === "ㅌㅂ") {
+      input(line)
     }
   }
 }
 
+const isPureNumber = (value: string) => {
+  const ㅋ = value.split("").filter((v) => v === "ㅋ").length
+  const ㅎ = value.split("").filter((v) => v === "ㅎ").length
+  if (ㅋ + ㅎ === value.length) return true
+  else return false
+}
+
 const getVariable = (line: string) => {
-  // 키워드는 변수이름이 될 수 없습니다
-  if (line.split(" ")[0] === "티비") return input(line)
+  if (line.split(" ")[0] === "ㅌㅂ") return input(line)
   if (statements.includes(line.split(" ")[0])) return
   const value = variables[line]
   if (!value) return toNumber(line) === 0 ? null : toNumber(line)
   return value
 }
 
-const allocateVariable = (line: string) => {
+const declareVariable = (line: string) => {
   const [statement, name, first, ...values] = line.split(" ")
-  if (statement !== "ㅇㅉ") return
-  if (
-    !name ||
-    name.length <= 0 ||
-    statements.includes(name) ||
-    !name
-      .split("")
-      .map((v) => {
-        if (!["어", "쩔"].includes(v)) return 1
-      })
-      .includes(1)
-  )
+  if (statement !== "어쩔") return
+  if (!name || name.length <= 0 || statements.includes(name) || isPureNumber(name))
     throw new Error("어쩔변수")
   let allocatingValue = ""
-  if (first === "티비") {
-    const inputValue = input([first, ...values].join(" "))
-    if (inputValue) allocatingValue = inputValue
+  if (first) {
+    if (first === "ㅌㅂ") {
+      const inputValue = input([first, ...values].join(" "))
+      if (inputValue) allocatingValue = inputValue
+    } else if (isPureNumber(first)) {
+      allocatingValue = String(toNumber([first, ...values].join(" ")))
+    } else {
+      allocatingValue = "0"
+    }
   } else {
-    allocatingValue = String(toNumber(first))
+    allocatingValue = "0"
   }
   variables[name] = allocatingValue
 }
 
 const assignVariable = (line: string) => {
   const [statement, name, ...values] = line.split(" ")
-  if (statement !== "ㅈㅉ") return
+  if (statement !== "저쩔") return
   if (!name || name.length <= 0) throw new Error("어쩔변수")
   const doesVariableExist = getVariable(name)
   if (doesVariableExist === null) throw new Error("어쩔변수")
   let value = ""
-  if (name === "티비") {
+  if (name === "ㅌㅂ") {
     const inputValue = input(values.join(" "))
     if (inputValue) value = inputValue
   } else {
@@ -77,20 +77,20 @@ const assignVariable = (line: string) => {
 }
 
 const toNumber = (line: string) => {
-  const pluses = line.split("").filter((v) => v === "어").length
-  const minuses = line.split("").filter((v) => v === "쩔").length
+  const pluses = line.split("").filter((v) => v === "ㅋ").length
+  const minuses = line.split("").filter((v) => v === "ㅎ").length
   return pluses - minuses
 }
 
 const print = (line: string) => {
   const [statement, ...printOut] = line.split(" ")
-  if (statement !== "어쩔") return
+  if (statement !== "ㅇㅉ") return
   console.log(printOut.map((v) => getVariable(v)).join(" "))
 }
 
 const input = (line: string) => {
   const [statement, ...inputString] = line.split(" ")
-  if (statement !== "티비") return
+  if (statement !== "ㅌㅂ") return
   const inputUser = ReadLine.question(inputString.join(" ") + "\n")
   return inputUser
 }
