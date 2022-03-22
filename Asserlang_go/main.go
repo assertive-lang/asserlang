1152f4c4b74d6ea544f1e1ffcc32ba4d8c2aebb8
Jeeho Ahn <io@fosshost.org>
2022-03-23 00:27:58 +0900

    feat: add parser errors

--- Asserlang_go/main.go
+++ Asserlang_go/main.go
@@ -6,7 +6,8 @@ import (
 	"io/ioutil"
 
 	"github.com/assertive-lang/asserlang/Asserlang_go/lexer"
-	"github.com/assertive-lang/asserlang/Asserlang_go/token"
+
+	"github.com/assertive-lang/asserlang/Asserlang_go/parser"
 )
 
 var FilePath string
@@ -22,7 +23,20 @@ func main() {
 		panic(err)
 	}
 	l := lexer.New(string(raw))
-	for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-		fmt.Printf("%+v\n", tok)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	if program == nil {
+		panic(fmt.Errorf("ParseProgram() returned nil"))
+	}
+	errors := p.Errors()
+	if len(errors) > 0 {
+		for _, msg := range errors {
+			fmt.Printf(msg)
+		}
+	}
+
+	for i, s := range program.Statements {
+		fmt.Printf("%d | %s\n", i+1, s)
 	}
 }
--- Asserlang_go/parser/parser.go
+++ Asserlang_go/parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/assertive-lang/asserlang/Asserlang_go/ast"
 	"github.com/assertive-lang/asserlang/Asserlang_go/lexer"
 	"github.com/assertive-lang/asserlang/Asserlang_go/token"
@@ -9,17 +11,27 @@ import (
 type Parser struct {
 	l *lexer.Lexer
 
+	errors    []string
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{l: l, errors: []string{}}
 
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -86,6 +98,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
